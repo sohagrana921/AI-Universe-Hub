bdeff6fbe0e10fb87071a54fcea623f32612b856
sohagrana921 <soharana921@gmail.com>
2023-03-04 16:51:30 +0600

    finnal modal

--- js/app.js
+++ js/app.js
@@ -101,107 +101,109 @@ const loadSingleData = (id) => {
 };
 // Show Single Data in A Modal
 const showDataModal = (singleDataDetails) => {
+  console.log(singleDataDetails);
   const modalContainer = document.getElementById("modal");
   modalContainer.innerHTML = "";
   modalContainer.innerHTML += `
-  <div
-  class="modal fade"
-  id="singleDataModal"
-  tabindex="-1"
-  aria-labelledby="exampleModalLabel"
-  aria-hidden="true"
->
-  <div class="modal-dialog modal-dialog-centered modal-xl">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="modal"
-          aria-label="Close"
-        ></button>
-      </div>
-      <div class="modal-body">
-        <div class="d-md-flex px-3 pb-3 gap-1 justify-content-between">
-          <div class="card w-50 border border-danger modal-right-card">
-            <div class="card-body">
-              <h5 class="card-title">${singleDataDetails.description}</h5>
-              <div class="row my-3">
-                <div class="pricing col-4">
-                  <p class="text-success fw-bold">
-                    <span>${
-                      singleDataDetails.pricing[0]
-                        ? singleDataDetails.pricing[0].price
-                        : "Free"
-                    }
-                      <br />
-                    </span>
-                    <span> Month <br /> </span>
-                    <span>${singleDataDetails.pricing[0].plan}</span>
-                  </p>
+  <div class="d-md-flex px-3 pb-3 gap-1 justify-content-between">
+                <div class="card w-50 border border-danger modal-right-card">
+                  <div class="card-body">
+                    <h5 class="card-title">${singleDataDetails.description}</h5>
+                    <div class="row my-3">
+                      <div class="pricing col-4">
+                        <p class="text-success fw-bold">
+                          <span>${
+                            singleDataDetails.pricing
+                              ? singleDataDetails.pricing[0].price
+                              : "Free"
+                          }
+                            <br />
+                          </span>
+                          <span> Month <br /> </span>
+                          <span>${
+                            singleDataDetails.pricing
+                              ? singleDataDetails.pricing[0].plan
+                              : ""
+                          }</span>
+                        </p>
+                      </div>
+                      <div class="pricing col-4">
+                        <p class="fw-bold text-warning-emphasis">
+                          <span>${
+                            singleDataDetails.pricing
+                              ? singleDataDetails.pricing[1].price
+                              : ""
+                          }</span> <br />
+                          Month <br />
+                          <span>${
+                            singleDataDetails.pricing
+                              ? singleDataDetails.pricing[1].plan
+                              : ""
+                          }</span>
+                        </p>
+                      </div>
+                      <div class="pricing col-4">
+                        <p class="fw-bold text-danger">
+                          Contact <br />
+                          Us for <br />
+                          <span>${
+                            singleDataDetails.pricing
+                              ? singleDataDetails.pricing[2].plan
+                              : ""
+                          }</span>
+                        </p>
+                      </div>
+                    </div>
+                    <div class="d-md-flex px-2 gap-1 justify-content-between">
+                      <div>
+                        <h5>Features</h5>
+                        <ul>
+                          <li>${singleDataDetails.features[1].feature_name}</li>
+                          <li>${singleDataDetails.features[2].feature_name}</li>
+                          <li>${singleDataDetails.features[3].feature_name}</li>
+                        </ul>
+                      </div>
+                      <div>
+                        <h5>Integrations</h5>
+                        <p">
+                        <ul type="1">
+                        ${
+                          singleDataDetails.integrations
+                            ? singleDataDetails.integrations
+                                .map((item) => `<li>${item}</li>`)
+                                .join("")
+                            : "No data found"
+                        }
+                      </ul>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div class="pricing col-4">
-                  <p class="fw-bold text-warning-emphasis">
-                    <span>${singleDataDetails.pricing[1].price}</span> <br />
-                    Month <br />
-                    <span>${singleDataDetails.pricing[1].plan}</span>
-                  </p>
-                </div>
-                <div class="pricing col-4">
-                  <p class="fw-bold text-danger">
-                    Contact <br />
-                    Us for <br />
-                    <span>${singleDataDetails.pricing[2].plan}</span>
-                  </p>
-                </div>
-              </div>
-              <div class="d-md-flex px-2 gap-1 justify-content-between">
-                <div>
-                  <h5>Features</h5>
-                  <ul>
-                    <li>${singleDataDetails.features[1].feature_name}</li>
-                    <li>${singleDataDetails.features[2].feature_name}</li>
-                    <li>${singleDataDetails.features[3].feature_name}</li>
-                  </ul>
-                </div>
-                <div>
-                  <h5>Integrations</h5>
-                  <p">
-                  <ul type="1">
-                  ${singleDataDetails.integrations
-                    .map((item) => `<li>${item}</li>`)
-                    .join("")}
-                </ul>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div">
-          <div class="card" style="width: 340px; height:100%">
-  <img src="${
-    singleDataDetails.image_link[0]
-  }" class="card-img-top p-3 " alt="..." />
-  <a href="#" style="margin-left:200px;" id="accuracy-btn" class="btn btn-danger position-absolute mt-2 me-n5" >${
-    singleDataDetails.accuracy.score > "0"
-      ? singleDataDetails.accuracy.score * 100
-      : (document.getElementById("accuracy-btn").style.display = "none")
-  }% accuracy</a>
-  <div class="card-body text-center">
-    <h5 class="card-title">${
-      singleDataDetails.input_output_examples[0].input
-    }</h5>
-    <p class="card-text">${
-      singleDataDetails.input_output_examples[0].output
-    } </p>
-  </div>
- </div>
-          </div>
+                <div">
+                <div class="card" style="width: 340px; height:100%">
+        <img src="${
+          singleDataDetails.image_link[0]
+        }" class="card-img-top p-3 " alt="..." />
+        <a href="#" style="margin-left:200px;" id="accuracy-btn" class="btn btn-danger position-absolute mt-2 me-n5" >${
+          singleDataDetails.accuracy.score
+            ? singleDataDetails.accuracy.score * 100
+            : "0"
+        }% accuracy</a>
+        <div class="card-body text-center">
+          <h5 class="card-title">${
+            singleDataDetails.input_output_examples
+              ? singleDataDetails.input_output_examples[0].input
+              : ""
+          }</h5>
+          <p class="card-text">${
+            singleDataDetails.input_output_examples
+              ? singleDataDetails.input_output_examples[0].output
+              : "No Example"
+          } </p>
         </div>
-      </div>
-      <div class="modal-footer"></div>
-    </div>
-  </div>
-</div>
+       </div>
+                </div>
+              </div>
   `;
 };
